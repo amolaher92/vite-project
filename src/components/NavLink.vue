1bd548fd67e8a2c73ff376d4845811bafb87dcc1
Amol_Aher <amolaher92@gmail.com>
2022-01-26 00:54:23 +0530

    created navlink component

---
+++ src/components/NavLink.vue
@@ -0,0 +1,20 @@
+<template>
+    <ul class="list-reset lg:flex justify-end flex-1 items-center">
+        <template v-for="(link,index) in links" :key="index">
+            <li class="mr-3">
+                <router-link class="nav-link" :to="{name:link.slug}">{{link.label}}</router-link>
+            </li>
+        </template>
+    </ul>
+</template>
+<script>
+export default {
+    name: "NavLink",
+    props: {
+        links: {
+            type: Array,
+            required: true
+        },
+    }
+}
+</script>
