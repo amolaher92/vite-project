2b136284550042061e7ca3c5965b7d4dcbf08ac6
Amol_Aher <amolaher92@gmail.com>
2022-01-26 00:54:41 +0530

    create navbar layout component

---
+++ src/layout/NavbarLayout.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav class="fixed w-full z-30 top-0 py-5 px-2">
+        <div class="container w-full mx-auto flex flex-wrap items-center justify-between mt-0">
+            <div class="pl-4 flex items-center">
+                <slot name="brand"></slot>
+            </div>
+            <slot></slot>
+        </div>
+    </nav>
+</template>
+<script>
+export default {
+    name: "NavbarLayout"
+}
+</script>
