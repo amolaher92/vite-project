391af7c80cf1b7736ca55bacec742b61afeb2bd7
Amol_Aher <amolaher92@gmail.com>
2022-01-26 00:54:53 +0530

    updated all menus

---
+++ src/router/Menus.js
@@ -0,0 +1,23 @@
+const Menus = [{
+        label: "Home",
+        slug: "Home"
+    },
+    {
+        label: "Experience",
+        slug: "Experience"
+    },
+    {
+        label: "Technical Skills",
+        slug: "TechnicalSkills"
+    },
+    {
+        label: "Educations",
+        slug: "Educations"
+    },
+    {
+        label: "About",
+        slug: "About"
+    }
+];
+
+export default Menus;
