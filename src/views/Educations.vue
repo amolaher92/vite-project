55fa840c6de0a3c4b963c6b2e14ddcecd83ca07a
Amol_Aher <amolaher92@gmail.com>
2022-01-26 00:55:55 +0530

    created new required files

---
+++ src/views/Educations.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1 class="text-3xl text-blue-700">Educations</h1>
+</template>
+<script>
+export default{
+	name:"Educations",
+	data(){
+		return{
+
+		}
+	}
+}
+</script>
+<style lang="sass" scoped>
+	
+</style>
---
+++ src/views/TechnicalSkills.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1 class="text-3xl text-blue-700">Technical Skills</h1>
+</template>
+<script>
+export default{
+	name:"TechnicalSkills",
+	data(){
+		return{
+
+		}
+	}
+}
+</script>
+<style lang="sass" scoped>
+	
+</style>
