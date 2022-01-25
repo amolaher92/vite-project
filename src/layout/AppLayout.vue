4a0f0a2751e90635fb1823f41019de55107de4f4
Amol_Aher <amolaher92@gmail.com>
2022-01-26 00:53:22 +0530

    App layout update

--- src/layout/AppLayout.vue
+++ src/layout/AppLayout.vue
@@ -1,23 +1,23 @@
 <template>
-  <div class="leading-normal tracking-normal bg-gradient-to-r from-pink-500 to-orange-500">
-    <app-header></app-header>
-    <slot></slot>
-    <app-footer></app-footer>
-  </div>
+    <div class="leading-normal tracking-normal bg-gradient-to-r from-pink-500 to-orange-500">
+        <app-header></app-header>
+        <!-- <the-navbar></the-navbar> -->
+        <slot></slot>
+        <app-footer></app-footer>
+    </div>
 </template>
-
 <script>
 import AppHeader from '@/components/AppHeader.vue';
 import AppFooter from '@/components/AppFooter.vue';
-
+// import TheNavbar from '@/components/Navbar.vue';
 export default {
-  name: "AppLayout",
-  components: {
-    AppHeader, AppFooter
-  }
+    name: "AppLayout",
+    components: {
+        AppHeader, 
+        AppFooter,
+        // TheNavbar
+    }
 }
 </script>
-
-<style scoped>
-
+<style lang="sass" scoped>
 </style>
